cf9ef23593b90196d5ce71223fee16ab2b0ebc0e
Vitor A. Machado <116811472+vitorvitoor@users.noreply.github.com>
2024-08-15 09:04:30 -0300

    Add files via upload

---
+++ DesafioLogicos.java
@@ -0,0 +1,21 @@
+package Fundamentos;
+
+public class DesafioLogicos {
+	
+	public static void main(String[] args) {
+		boolean trabalho1 = true;
+		boolean trabalho2 = false; 
+		
+		boolean comprouTV50 = trabalho1 && trabalho2;
+		boolean comprouTV32 = trabalho1 ^ trabalho2;
+		boolean comprouSorvete = trabalho1 || trabalho2;
+		
+		boolean maisSaudavel = !comprouSorvete;
+		
+		System.out.println("Comprou TV 50\"?" + comprouTV50 );
+		System.out.println("Comprou TV 32\"?" + comprouTV32);
+		System.out.println("Comprou Sorvete?" + comprouSorvete);
+		System.out.println("Mais saudável?" + maisSaudavel);
+
+	}
+}
